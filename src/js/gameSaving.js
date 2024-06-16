b396544760b44d9b6d10c396a252b4195e115135
Serg1506z <136631034+Serg1506z@users.noreply.github.com>
2024-06-16 17:48:59 +0300

    add files

--- src/js/gameSaving.js
+++
@@ -1,9 +0,0 @@
-import read from "./render";
-import json from "./parser";
-export default class GameSavingLoader {
-   static async load() {
-    const data = await read(); // возвращается Promise!
-    const value = await json(data); // возвращается Promise!
-    return value;
-  }
-}
